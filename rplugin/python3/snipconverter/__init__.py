2a351a9024de4d13b44a0be32692dc2981b6de61
SecretCat <luckyhiroaki@icloud.com>
2021-04-06 01:18:24 +0000

    update

--- rplugin/python3/snipconverter/__init__.py
+++ rplugin/python3/snipconverter/__init__.py
@@ -1,11 +1,18 @@
 import pynvim
+from pathlib import Path
 from snipconverter.util.vars import Vars
+from snipconverter.converter import Converter
 
 @pynvim.plugin
 class SnipConverter(object):
     def __init__(self, nvim):
         self.nvim = nvim
+        self.plugin_path = Path(__file__).parent.parent.parent.parent
+        self.vsnip_dir = str(self.plugin_path) + r"/snippets/vsnip"
+        self.snip_dir = str(self.plugin_path) + r"/snippets/snip"
         self.vars = util.vars.Vars(nvim)
+        self.converter = Converter(vsnips_dir=)
+
 
     @pynvim.command('Damy')
     def damy(self):
